74f8a165dd64583f4563cbd0d7b84511116bfc5a
Sekharyadav2027 <sekharyadav20071@gmail.com>
2026-03-02 12:49:15 +0530

    added gunicorn

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
@@ -10,19 +11,4 @@ def home():
     """
 
 if __name__ == "__main__":
-    app.run(debug=True)
-
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return "Hello World"
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
